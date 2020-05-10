f692cfaecb34cb27a9805b8be5bd72ee6c88ec89
Nuwan Karunarathna <nuwankarunarathna@hotmail.com>
2020-05-10 21:40:01 +0530

    [FEAT] Added responive navigation drawer for the mobile version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,89 @@
 import React from "react";
 import "../styles/Header.css";
 import { getAssetUrl } from "../helpers/assets.helper";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMobileSideBar } from "../redux/actions/ui.actions";
+
+const Logo = () => (
+  <div className="logo-wrap flex-center">
+    <div className="logo">
+      <img src={getAssetUrl("/logo/logo-text-accent.png")} alt="logo" />
+    </div>
+  </div>
+);
+
+const SearchBox = ({ onSearchSubmit }) => {
+  const handleKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      // enter pressed
+      onSearchSubmit(e.target.value);
+    }
+  };
+
+  return (
+    <div id="search-box" className="search-box-wrap">
+      <input className="search-box" placeholder="Search for items and brands" />
+      <span className="search-icon">
+        <i class="fas fa-search" onKeyUp={handleKeyUp}></i>
+      </span>
+    </div>
+  );
+};
+
+const CustomerMenu = () => {
+  return (
+    <div className="customer-menu">
+      <div className="customer-avatar">
+        <span>Login</span>
+      </div>
+    </div>
+  );
+};
+
+const HeaderButtonGroup = () => {
+  return (
+    <div className="header-btn-group flex">
+      <button id="m-search-btn" className="header-btn">
+        <i class="fas fa-search"></i>
+      </button>
+      <button className="header-btn">
+        <i class="far fa-heart"></i>
+      </button>
+      <button className="header-btn">
+        <i class="fas fa-shopping-basket"></i>
+      </button>
+      <CustomerMenu />
+    </div>
+  );
+};
+
+const MobileNavBarBtn = () => {
+  const dispatch = useDispatch();
+  const sideBarOpened = useSelector((state) => state.ui.mobileSideBarOpened);
+
+  return (
+    <button
+      id="m-nav-drawer-btn"
+      className="header-btn nav-drawer-btn"
+      onClick={() => dispatch(toggleMobileSideBar())}
+    >
+      {sideBarOpened ? (
+        <i class="fas fa-times"></i>
+      ) : (
+        <i class="fas fa-bars"></i>
+      )}
+    </button>
+  );
+};
 
 export const Header = () => {
-  const styles = {
-    header: {
-      padding: "10px 10px 0px 10px",
-    },
-    logo: {
-      width: "80px",
-      height: "80px",
-    },
-    logoImg: {
-      width: "100%",
-      //   height: "100%",
-    },
-  };
   return (
-    <div className="header-wrap flex" style={styles.header}>
-      <div className="logo" style={styles.logo}>
-        <img
-          style={styles.logoImg}
-          src={getAssetUrl("/logo/logo-text-accent.png")}
-          alt="logo"
-        />
-      </div>
+    <div className="header-wrap flex">
+      <MobileNavBarBtn />
+      <Logo />
+      <SearchBox />
+
+      <HeaderButtonGroup />
     </div>
   );
 };
--- src/pages/customer/Homepage.js
+++ src/pages/customer/Homepage.js
@@ -3,15 +3,20 @@ import { Header } from "../../components/Header";
 
 import "../../styles/CustomerShell.css";
 import CategoryBar from "../../components/customer/CategoryBar";
+import { useSelector } from "react-redux";
 
 export default function Homepage() {
+  const sideBarOpened = useSelector((state) => state.ui.mobileSideBarOpened);
+
   return (
     <div className="customer-shell flex flex-r">
       <div className="w-100 flex flex-c">
         <Header />
 
         <div className="content-wrap flex">
-          <div className="category-wrap">
+          <div
+            className={`category-wrap ${sideBarOpened ? "opened" : "closed"}`}
+          >
             <CategoryBar />
           </div>
 
