68cf9b9aff558155a9da31a1cd5982210f17d1f0
Nuwan Karunarathna <nuwankarunarathna@hotmail.com>
2020-05-10 23:28:16 +0530

    [FEAT] Added customer info to the header and the customer dashboard sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Header.css";
 import { getAssetUrl } from "../helpers/assets.helper";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMobileSideBar } from "../redux/actions/ui.actions";
+import { Link, useHistory } from "react-router-dom";
+import { ROUTE_PATHS } from "../constants";
+import { logoutAsync } from "../redux/actions/customer/customer.actions";
 
 const Logo = () => (
   <div className="logo-wrap flex-center">
-    <div className="logo">
-      <img src={getAssetUrl("/logo/logo-text-accent.png")} alt="logo" />
-    </div>
+    <Link to={ROUTE_PATHS.CUSTOMER_SHELL}>
+      <div className="logo">
+        <img src={getAssetUrl("/logo/logo-text-accent.png")} alt="logo" />
+      </div>
+    </Link>
   </div>
 );
 
@@ -24,17 +29,85 @@ const SearchBox = ({ onSearchSubmit }) => {
     <div id="search-box" className="search-box-wrap">
       <input className="search-box" placeholder="Search for items and brands" />
       <span className="search-icon">
-        <i class="fas fa-search" onKeyUp={handleKeyUp}></i>
+        <i className="fas fa-search" onKeyUp={handleKeyUp}></i>
       </span>
     </div>
   );
 };
 
+const CustomerAvatar = () => {
+  const customerInfo = useSelector((state) => state.customer.customerInfo);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [menuExpanded, setMenuExpanded] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (
+        e.target &&
+        e.target.classList &&
+        !e.target.classList.contains("customer-menu-item")
+      ) {
+        setMenuExpanded(false);
+      }
+    });
+
+    history.listen(() => {
+      setMenuExpanded(false);
+    });
+  }, []);
+
+  const handleDashboardBtnClicked = () => {
+    history.push(ROUTE_PATHS.CUSTOMER_DASHBOARD);
+  };
+  const handleLogoutClicked = () => {
+    dispatch(logoutAsync(history));
+  };
+
+  return (
+    <div className="customer-avatar-wrap">
+      <div
+        className={`customer-info`}
+        onClick={() => setMenuExpanded(!menuExpanded)}
+      >
+        <span>{customerInfo && customerInfo.fName}</span>
+      </div>
+
+      {menuExpanded && (
+        <div className="customer-expand-menu">
+          <div className="customer-menu">
+            <div
+              className="customer-menu-item"
+              onClick={handleDashboardBtnClicked}
+            >
+              <span>Dashboard</span>
+            </div>
+            <div className="customer-menu-item" onClick={handleLogoutClicked}>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
 const CustomerMenu = () => {
+  const token = useSelector((state) => state.customer.token);
+
   return (
     <div className="customer-menu">
       <div className="customer-avatar">
-        <span>Login</span>
+        {token ? (
+          <CustomerAvatar />
+        ) : (
+          <Link to={ROUTE_PATHS.CUSTOMER_LOGIN}>
+            <div className="customer-menu-login-btn">
+              <span>Login</span>
+            </div>
+          </Link>
+        )}
       </div>
     </div>
   );
@@ -47,10 +120,10 @@ const HeaderButtonGroup = () => {
         <i class="fas fa-search"></i>
       </button>
       <button className="header-btn">
-        <i class="far fa-heart"></i>
+        <i className="far fa-heart"></i>
       </button>
       <button className="header-btn">
-        <i class="fas fa-shopping-basket"></i>
+        <i className="fas fa-shopping-basket"></i>
       </button>
       <CustomerMenu />
     </div>
@@ -68,9 +141,9 @@ const MobileNavBarBtn = () => {
       onClick={() => dispatch(toggleMobileSideBar())}
     >
       {sideBarOpened ? (
-        <i class="fas fa-times"></i>
+        <i className="fas fa-times"></i>
       ) : (
-        <i class="fas fa-bars"></i>
+        <i className="fas fa-bars"></i>
       )}
     </button>
   );
--- src/pages/customer/Homepage.js
+++ src/pages/customer/Homepage.js
@@ -4,10 +4,36 @@ import { Header } from "../../components/Header";
 import "../../styles/CustomerShell.css";
 import CategoryBar from "../../components/customer/CategoryBar";
 import { useSelector } from "react-redux";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import { ROUTE_PATHS } from "../../constants";
+import CustomerDashboardPage from "./CustomerDashboardPage";
+import CustomerDashboardSideBar from "../../components/customer/CustomerDashboardSideBar";
+
+function PrivateRoute({ children, ...rest }) {
+  const token = useSelector((state) => state.customer.token);
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        token !== null ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: ROUTE_PATHS.CUSTOMER_LOGIN,
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 export default function Homepage() {
   const sideBarOpened = useSelector((state) => state.ui.mobileSideBarOpened);
-
+  const location = useLocation();
   return (
     <div className="customer-shell flex flex-r">
       <div className="w-100 flex flex-c">
@@ -17,11 +43,21 @@ export default function Homepage() {
           <div
             className={`category-wrap ${sideBarOpened ? "opened" : "closed"}`}
           >
-            <CategoryBar />
+            {location.pathname.includes(ROUTE_PATHS.CUSTOMER_DASHBOARD) ? (
+              <CustomerDashboardSideBar />
+            ) : (
+              <CategoryBar />
+            )}
           </div>
 
           <div className="page-content-wrap">
-            <div className="page"></div>
+            <div className="page">
+              <Switch>
+                <PrivateRoute path={ROUTE_PATHS.CUSTOMER_DASHBOARD}>
+                  <CustomerDashboardPage />
+                </PrivateRoute>
+              </Switch>
+            </div>
 
             <div className="footer-wrap"></div>
           </div>
