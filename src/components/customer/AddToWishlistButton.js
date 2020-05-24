67ecd47436e4f553b60f71d777153d0e22c7ef48
Nuwan Karunarathna <nuwankarunarathna504@gmail.com>
2020-05-24 19:00:59 +0530

    [FEAT/UP] Updated wishlist button to restrict adding items to the wishlist which are already in it

--- src/components/customer/AddToWishlistButton.js
+++ src/components/customer/AddToWishlistButton.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { ROUTE_PATHS } from "../../constants";
 import { addProductToWishlistAsync } from "../../redux/actions/customer/wishlist.action";
+import { displayToastAsync } from "../../redux/actions/toast.actions";
+import { buildNotification, NOTIFICATION_TYPE } from "../../services/customer/notification.service";
 
-export default function AddToWishlistButton( { productId }) {
+export default function AddToWishlistButton({ productId }) {
     const dispatch = useDispatch();
     const { token } = useSelector((state) => state.customer);
+    const { items } = useSelector((state) => state.wishlist);
     const history = useHistory();
 
+    const isProductInWishlistAlready = useMemo(() => {
+        const productIds = items.map(i => i.product._id);
+        return productIds.includes(productId);
+    }, [productId, items])
+
     const onClickHandler = () => {
-        if(!token) {
+        if (!token) {
             history.push(ROUTE_PATHS.CUSTOMER_LOGIN);
+            return;
+        }
+        if (isProductInWishlistAlready) {
+            dispatch(displayToastAsync(buildNotification("Product is already in the wishlist", NOTIFICATION_TYPE.ERROR)))
             return;
         }
         dispatch(addProductToWishlistAsync(productId));
     };
-    return(
+    return (
         <button className="wishlist-btn" onClick={onClickHandler}>
             <i class="on-hover fas fa-heart"></i>
             <i class="default far fa-heart"></i>
