7fb71126583f81c62c830c11aea607aecce622fa
Nuwan Karunarathna <nuwankarunarathna504@gmail.com>
2020-05-24 19:41:21 +0530

    [FEAT] Added go home button to the login page

--- src/components/customer/CustomerSignUpMsg.js
+++ src/components/customer/CustomerSignUpMsg.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "../../styles/CustomerLoginPage.css";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { ROUTE_PATHS } from "../../constants";
 
 export default function CustomerSignUpMsg() {
     const history = useHistory();
     return (
-        <div className="sign-up-msg-wrapper"> 
+        <div className="sign-up-msg-wrapper">
             <div className="sign-up-msg">
                 <p>Hi, There!</p>
                 <p>
-                    Enter your personal details 
+                    Enter your personal details
                     and start journey with us.
                 </p>
                 <div className="sign-up-msg-btn">
@@ -18,8 +18,14 @@ export default function CustomerSignUpMsg() {
                         history.push(ROUTE_PATHS.CUSTOMER_SIGNUP);
                     }}>SIGN UP</button>
                 </div>
+
+                <Link to={ROUTE_PATHS.CUSTOMER_SHELL}>
+                    <button className="go-home-btn">
+                        Go to Homepage
+                </button>
+                </Link>
             </div>
-        </div> 
+        </div>
     );
 }
 
